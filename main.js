5e7214b07629adacfee337b2f8276b1ddae72e03
Byron McMullen <bmcmullen@whitepages.com>
2015-03-27 10:15:55 -0700

    most simple repro

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 var usagejs = require('usagejs');
 var three = require('three');
 var moment = require('moment');
-var underscore = require('underscore');
-var Period = require('date-period');
 
 window.Usage = usagejs['Usage'];
